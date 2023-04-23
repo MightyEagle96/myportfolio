b2807f673d326fd14cf0b246a2c4331d534d8885
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2023-04-23 23:48:00 +0100

    made changes

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,23 +1,98 @@
 import React from "react";
 import logo from "../image/officialLogo2.png";
-import { Link, Typography } from "@mui/material";
-import { Email } from "@mui/icons-material";
+import { Link, Stack, Typography } from "@mui/material";
+import { Email, Facebook, Twitter } from "@mui/icons-material";
 export default function Footer() {
   return (
     <div className="footer">
       <div className="container">
-        <div className="row text-white">
-          <div className="col-lg-6 mb-2">
-            <img src={logo} className="img-fluid" />
+        <div className="d-none d-lg-block">
+          <div className="row text-white">
+            <div className="col-lg-6 mb-2">
+              <img src={logo} className="img-fluid" alt="logo" />
+            </div>
+            <div className="col-lg-6 d-flex align-items-center mb-2">
+              <div>
+                <div className="mb-3">
+                  <Link
+                    underline="none"
+                    sx={{ color: "white", ":hover": { color: "whitesmoke" } }}
+                    href="mailto: devmightyeagle@gmail.com"
+                  >
+                    <Email /> SEND AN EMAIL
+                  </Link>
+                </div>
+                <div className="mb-1">
+                  <Stack direction="row" spacing={2}>
+                    <div>
+                      <Link
+                        underline="none"
+                        sx={{
+                          color: "white",
+                          ":hover": { color: "whitesmoke" },
+                        }}
+                        href="https://web.facebook.com/TheMightyEagle.CEO"
+                      >
+                        <Facebook />
+                      </Link>
+                    </div>
+                    <div>
+                      <Link
+                        underline="none"
+                        sx={{
+                          color: "white",
+                          ":hover": { color: "whitesmoke" },
+                        }}
+                        href="https://twitter.com/TheMightyEagle7"
+                      >
+                        <Twitter />
+                      </Link>
+                    </div>
+                  </Stack>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="col-lg-6 d-flex align-items-center mb-2">
-            <div className="mb-1">
+        </div>
+        <div className="d-sm-block d-md-none">
+          <div className="d-flex justify-content-center">
+            <img src={logo} style={{ height: 100, width: 150 }} alt="logo" />
+          </div>
+          <div>
+            <div className="text-center">
               <Link
-                sx={{ color: "white", backgroundColor: "white" }}
+                underline="none"
+                sx={{ color: "white", ":hover": { color: "whitesmoke" } }}
                 href="mailto: devmightyeagle@gmail.com"
               >
-                SEND AN EMAIL
+                <Email /> SEND AN EMAIL
               </Link>
+            </div>
+            <div className="d-flex justify-content-center">
+              <div className="me-2">
+                <Link
+                  underline="none"
+                  sx={{
+                    color: "white",
+                    ":hover": { color: "whitesmoke" },
+                  }}
+                  href="https://web.facebook.com/TheMightyEagle.CEO"
+                >
+                  <Facebook />
+                </Link>
+              </div>
+              <div>
+                <Link
+                  underline="none"
+                  sx={{
+                    color: "white",
+                    ":hover": { color: "whitesmoke" },
+                  }}
+                  href="https://twitter.com/TheMightyEagle7"
+                >
+                  <Twitter />
+                </Link>
+              </div>
             </div>
           </div>
         </div>
